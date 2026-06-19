bedc469f6505b14b9d2c23b21da5b53364204720
Deyvid Godoy <deyvidjgv12@gmail.com>
2026-06-18 19:58:44 -0500

    agregar habilidades

--- main.js
+++ main.js
@@ -25,18 +25,36 @@ const pokemonSelected = async (pokemonUrl) => {
         const pokemonImage = document.getElementById("pokemon-image");
         const pokemonName = document.getElementById("pokemon-name");
         const pokemonStats = document.getElementById("pokemon-stats");
+        
 
         pokemonImage.src = response.sprites.front_default;
         pokemonName.textContent = response.name;
 
-        pokemonStats.innerHTML = "";
+        pokemonStats.innerHTML = "<h2>Estadisticas</h2>";
 
         response.stats.forEach(stat => {
             const li = document.createElement("li");
             li.textContent = `${stat.stat.name}: ${stat.base_stat}`;
             pokemonStats.appendChild(li);
 
+        
         })
+        const pokemonAbilities = document.getElementById("pokemon-abilities");
+        const abilities = document.getElementById("abilities");
+        abilities.textContent = "Habilidades";
+        pokemonAbilities.innerHTML = "";
+
+        response.abilities.length
+
+        response.abilities.forEach(ability => {    
+            console.log(ability.ability.name);
+            const li = document.createElement("li");
+            li.textContent = ability.ability.name;
+
+            pokemonAbilities.appendChild(li);
+        })
+
+
     } catch (error) {
         console.error("Error fetching pokemon details:", error);
     }
